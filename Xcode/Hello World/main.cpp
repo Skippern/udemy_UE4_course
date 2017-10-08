d6aafed4ae53f249ab1c60369fa5ec99e065a077
Aun Johnsen <lists@gimnechiske.org>
2017-10-08 23:53:56 +0300

    Xcode Hello World
    Hello World C++ from Xcode

---
+++ Xcode/Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Aun Johnsen on 10/8/17.
+//  Copyright © 2017 Aun Johnsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello World\n";
+    return 0;
+}
