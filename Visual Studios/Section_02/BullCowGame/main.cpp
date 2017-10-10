c0351f98120c94e76d62ca3d6f314249a0140e61
Aun Johnsen <Skippern@users.noreply.github.com>
2017-10-10 21:09:30 +0300

    Section 2 Lecture 22

--- Visual Studios/Section_02/BullCowGame/main.cpp
+++ Visual Studios/Section_02/BullCowGame/main.cpp
@@ -6,7 +6,22 @@
 
 using namespace std;
 
+// Prototypes
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// The entry point for our application
 int main()
+{
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;
+}
+
+void PrintIntro()
 {
 	// ubtriduce the game
 	constexpr int WORD_LENGTH = 5;
@@ -15,15 +30,20 @@ int main()
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
 
+	return;
+}
+
+string GetGuessAndPrintBack()
+{
 	// get a guess from the player
 	string Guess = "";
 	cout << endl << "Enter your guess: ";
-	cin >> Guess;
+	getline(cin, Guess);
 	cout << endl;
 
 	// repeat the guess back to them
 	cout << "You guessed: " << Guess << endl;
 
-	cout << endl;
-	return 0;
+	return Guess;
 }
+
