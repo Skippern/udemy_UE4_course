e408a5282092e82a36c8559a91e88b021e094d1c
Aun Johnsen <lists@gimnechiske.org>
2017-10-10 21:12:14 +0300

    More Cow Game

--- Xcode/BullCowGame/BullCowGame/main.cpp
+++ Xcode/BullCowGame/BullCowGame/main.cpp
@@ -10,22 +10,42 @@
 
 using namespace std;
 
+// Prototypes
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
+// entry point for our application
 int main(int argc, const char * argv[]) {
-    // introduce the game
+    
+    PrintIntro();
+    
+    string Guess = GetGuessAndPrintBack();
+
+    
+    cout << endl;
+    return 0;
+}
+
+// introduce the game
+void PrintIntro() {
     constexpr int WORD_LENGTH = 5;
     
     cout << "Welcome to Bulls and Cows, a fun word game\n";
     cout << "Can you guess the " << WORD_LENGTH;
     cout << " letter isogram I'm thinking of?\n";
+    
+    return;
+}
 
-    // get a guess from the player
+// get a guess from the player
+string GetGuessAndPrintBack() {
     string Guess = "";
     cout << "\nPlace your guess: ";
-    cin >> Guess;
+    getline(cin, Guess);
     cout << endl;
     // repeat the guess back to them
     cout << "You guessed: " << Guess << endl;
-    
-    cout << endl;
-    return 0;
+
+    return Guess;
 }
